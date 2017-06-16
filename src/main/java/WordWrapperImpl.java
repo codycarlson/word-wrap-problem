8a99c500ef25e9803250b4e59fe5afbf19c948b9
Interview <Interview>
2017-06-16 11:13:52 -0400

    add pair programming exercise for zachary mcnulty

--- src/main/java/WordWrapperImpl.java
+++ src/main/java/WordWrapperImpl.java
@@ -3,6 +3,26 @@ import java.util.List;
 
 public class WordWrapperImpl implements WordWrapper {
     public List<String> wrapWords(String string, int maximumCharactersPerLine) {
-        return new ArrayList<String>();
+        ArrayList<String> ListOfLines = new ArrayList<>();
+        String [] split = string.split(" ");
+        String temp = "";
+        int x = 1;
+        for(String s: split){
+            if((temp.length() + s.length()) <= maximumCharactersPerLine){
+                temp += s;
+                if(temp.length() + 1 <= maximumCharactersPerLine) {
+                    temp += " ";
+                }
+                if(x == split.length){
+                    ListOfLines.add(temp.trim());
+                }
+            }else{
+                ListOfLines.add(temp.trim());
+                temp = s + " ";
+            }
+            x++;
+        }
+
+        return ListOfLines;
     }
 }
