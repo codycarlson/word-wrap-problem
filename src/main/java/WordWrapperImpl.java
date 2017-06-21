e977d1b215cede112f7be4ea2ec1c2b20608aa6a
Brian Donohoe <bdonohoe@commercehub.com>
2017-06-20 18:09:37 -0700

    Revert "add pair programming exercise for zachary mcnulty"
    This reverts commit 8a99c500ef25e9803250b4e59fe5afbf19c948b9.

--- src/main/java/WordWrapperImpl.java
+++ src/main/java/WordWrapperImpl.java
@@ -3,26 +3,6 @@ import java.util.List;
 
 public class WordWrapperImpl implements WordWrapper {
     public List<String> wrapWords(String string, int maximumCharactersPerLine) {
-        ArrayList<String> ListOfLines = new ArrayList<>();
-        String [] split = string.split(" ");
-        String temp = "";
-        int x = 1;
-        for(String s: split){
-            if((temp.length() + s.length()) <= maximumCharactersPerLine){
-                temp += s;
-                if(temp.length() + 1 <= maximumCharactersPerLine) {
-                    temp += " ";
-                }
-                if(x == split.length){
-                    ListOfLines.add(temp.trim());
-                }
-            }else{
-                ListOfLines.add(temp.trim());
-                temp = s + " ";
-            }
-            x++;
-        }
-
-        return ListOfLines;
+        return new ArrayList<String>();
     }
 }
--- src/test/groovy/WordWrapperImplTest.groovy
+++ src/test/groovy/WordWrapperImplTest.groovy
@@ -4,14 +4,9 @@ class WordWrapperImplTest extends Specification{
     def "Insert meaningful test name here"() {
         given:
             //set up a thing
-            def wrapper = new WordWrapperImpl();
-            def testString = "The Quick Brown Fox Jumps Over The Lazy Dog";
         when:
             //we do a thing
-            def LineList = wrapper.wrapWords(testString, 8);
         then:
-//            LineList == testString.split(" ");
-            LineList == ["The", "Quick", "Brown", "Fox", "Jumps", "Over The", "Lazy Dog"];
             true
             //the thing that we thought would happen, happened
     }
