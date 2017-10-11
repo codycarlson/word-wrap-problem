da5ff99f238dbee0bd8235a0c76380af72ba8d5a
Jonathan Lazzara <jlazzara@commercehub.com>
2017-10-11 11:58:04 -0400

    return null

--- src/main/java/WordWrapper.java
+++ src/main/java/WordWrapper.java
@@ -1,10 +1,9 @@
-import java.util.ArrayList;
 import java.util.List;
 
 public class WordWrapper {
 
     public List<String> wrapWords(String string, int maximumCharactersPerLine) {
 
-        return new ArrayList<String>();
+        return null;
     }
 }
