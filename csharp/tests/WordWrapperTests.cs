cbc31bd1e03015f2f1d706e68243eba720584038
Byung Kim <bkim@commercehub.com>
2017-11-27 13:57:35 -0800

    Add C#/.NET version of the interview question.

---
+++ csharp/tests/WordWrapperTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WordWrapper.Tests
+{
+	[TestClass]
+	public class WordWrapperTests
+	{
+		[TestMethod]
+		public void wrapWordsTest()
+		{
+			string sourceString = null;
+			int maximumCharactersPerLine = 0;
+
+			List<string> expected = null;
+			List<string> actual = WordWrapper.wrapWords(sourceString, maximumCharactersPerLine);
+
+			CollectionAssert.AreEqual(expected, actual);
+		}
+	}
+}
